af361e43583d2e9341819b1409b1350b4148d120
jakeburke1775 <jakeburke1775@gmail.com>
2025-10-31 15:49:31 -0500

    Simplify app - remove routing for easier GitHub Pages deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,18 @@
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header/Header';
 import Main from './components/Main/Main';
 import Footer from './components/Footer/Footer';
 import Home from './pages/Home/Home';
-import About from './pages/About/About';
-import Contact from './pages/Contact/Contact';
 import './App.css'
 
 function App() {
   return (
-    <Router>
-      <div className="app">
-        <Header />
-        <Main>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/contact" element={<Contact />} />
-          </Routes>
-        </Main>
-        <Footer />
-      </div>
-    </Router>
+    <div className="app">
+      <Header />
+      <Main>
+        <Home />
+      </Main>
+      <Footer />
+    </div>
   )
 }
 
